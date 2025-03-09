1f8e4caa15be13e8b3d2b6f4c5cc46b55eea1ecd
Delliott <dlelliott100@outlook.com>
2025-03-09 13:39:04 +0000

    don't need main task

--- pyepic/xmpp.py
+++ pyepic/xmpp.py
@@ -27,7 +27,6 @@ class XMPPWebsocketClient:
         "config",
         "session",
         "ws",
-        "main_task",
         "recv_task",
         "ping_task",
         "errors",
@@ -40,7 +39,6 @@ class XMPPWebsocketClient:
         self.session: ClientSession | None = None
         self.ws: ClientWebSocketResponse | None = None
 
-        self.main_task: Task | None = None
         self.recv_task: Task | None = None
         self.ping_task: Task | None = None
 
